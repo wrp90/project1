23009eeebc35e6f32b1444fe9fb198e7e447d08a
Will Papp <wpapp90@gmail.com>
2021-02-04 15:52:40 -0600

    finished fetch, can now get listings of adoptable pets

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,22 +14,39 @@ var getToken = function() {
 	headers: {
 		'Content-Type': 'application/x-www-form-urlencoded'
 	}
-}).then(function (response) {
+	}).then(function (response) {
 
-	// Return the response as JSON
-	return response.json();
+		// Return the response as JSON
+		return response.json();
 
-}).then(function (data) {
+	}).then(function (data) {
+		var org = "RI77";
+		var status = "adoptable";
+		// Log the API data
+		console.log('token', data);
+		
+		return fetch('https://api.petfinder.com/v2/animals?organization=' + org + '&status=' + status, {
+		headers: {
+			'Authorization': data.token_type + ' ' + data.access_token,
+			'Content-Type': 'application/x-www-form-urlencoded'
+		}
+	});
+	}).then(function (resp) {
 
-	// Log the API data
-	console.log('token', data);
+		// Return the API response as JSON
+		return resp.json();
 
-}).catch(function (err) {
+	}).then(function (data) {
 
-	// Log any errors
-	console.log('something went wrong', err);
+		// Log the pet data
+		console.log('pets', data);
 
-});
+	}).catch(function (err) {
+
+		// Log any errors
+		console.log('something went wrong', err);
+
+	});
 }
 
 getToken();
